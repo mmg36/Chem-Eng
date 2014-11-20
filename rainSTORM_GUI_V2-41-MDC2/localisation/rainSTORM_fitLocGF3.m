c7d6c131910181e3451d7a6082f599b5c16b1379
Mehdi <mmg36@cam.ac.uk>
2014-11-20 00:32:30 +0000

    Multiple molecule lioclisaitons fixed position final version

---
+++ rainSTORM_GUI_V2-41-MDC2/localisation/rainSTORM_fitLocGF3.m
@@ -0,0 +1,220 @@
+% Gausian Fit. Local Minimum Background Subtraction - not perfect! 
+% Halt After 3 Misfits (Wolter). 
+% Copyright 2012. Refer to 00_license.txt for details.
+% See, e.g. http://mathworld.wolfram.com/
+function [SupResParams] = rainSTORM_fitLocGF3(frameIdx, myFrame,myPixels,initX0,initSig,allowSig,rad,tol,allowX,bkgdSig,maxIts)
+%function [myFits,myParams] = rainSTORM_fitLocGF3(frameIdx, myFrame,myPixels,initX0,initSig,allowSig,rad,tol,allowX,bkgdSig,maxIts)
+persistent Nfails 
+
+% Determine if main localisation algorith will use old method (subtract
+% minimum of ROI) of the new method (subtract average of NON-ROI).
+% 0 = old method (Min(ROI)), 1 = new method (Mean(non-ROI))
+% fitting (fitting DC term to Gaussian)
+FlagState= 1;
+
+% To allow negative numbers for the background corrected counts, this
+% variable was changed to a double. 
+
+myFrame = double(myFrame);
+
+% Fit [x0,C,sigX] to make f(x)=C*exp( -(x-x0)^2/(2*sigX^2) ) 
+% Work on rows then cols. Reject fits with far-out x0, sigX, or residual.
+
+% myFits = -ones(size(myPixels,1),2);   % [row col] matrix for each candidate
+% myParams = -ones(size(myPixels,1),7); % Parameters for accepted fits
+SupResParams = [];
+index = 1;
+Nfails = 0; % Reset the number of rejected fits for this new frame
+
+% New method 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if FlagState == 1;
+  % ResBackground =(sum(sum(myFrame))-sum(sum(myROI)))/((size(myFrame,1)^2)-(size(myROI,1)^2)); % Background is average count outside ROI.
+   InitialBackground = mean (mean(double(myFrame)));
+   stdev = std(double(myFrame(:)));
+   area = (size(myFrame,1))^2;
+   SumBackground = 0; 
+   
+   for rowNum = 1: size(myFrame,1)
+       for colNum = 1 : size(myFrame, 1)
+           if myFrame(rowNum, colNum) < 3*stdev+InitialBackground 
+               SumBackground = SumBackground + double(myFrame(rowNum, colNum));
+           else 
+               area = area - 1;
+           end;
+       end;
+   end;
+   
+   ResBackground = SumBackground / area;
+  % ResBackground; 
+   % 'new method'
+end            
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for lpPx = 1:size(myPixels,1); % For local maxima in descending order
+myRow = myPixels(lpPx,1);
+myCol = myPixels(lpPx,2);
+myROI = myFrame(myRow-rad:myRow+rad,myCol-rad:myCol+rad); 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% "Old method"
+
+if FlagState == 0 ;
+    ResBackground = min(myROI(:));  % square region to fit. Subtract minimum.
+   % 'old method'
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+myROI = myROI - ResBackground;
+flagRowFits = false;   % Begin by noting the centre-position is not fitted
+flagColFits = false;
+
+xx = (-rad:rad)';      % x-positions (rows) (pixel widths) as column vector
+yy = (-rad:rad)';      % y-positions (cols) (pixel widths) as column vector
+yRows = sum(myROI,2);  % Sum all columns (dim=2) to get row intensities
+yCols = sum(myROI,1)'; % Sum all rows (dim=1) in each column.
+
+% Fit a Gaussian to the observed intensities, binned by row:
+x0 = initX0;
+sigX = initSig;
+C  = yRows(rad+1); % Guess height of f(x). Centre value is a good guess.
+  fofX = C*exp(-(xx-x0).^2/(2*sigX^2)); %+ D ; % Initial guess of f(x)
+  Beta = yRows - fofX; % Change needed in f(x)
+  for lpLSF = 1:maxIts
+  %if FlagState==2;
+      %d = ones  (size (xx,1),1); 
+      %A = [fofX/C,fofX.*(xx-x0)/sigX^2,fofX.*(xx-x0).^2/sigX^3,d]; % Jacobian
+  %else
+   A = [fofX/C,fofX.*(xx-x0)/sigX^2,fofX.*(xx-x0).^2/sigX^3]; % Jacobian
+  %end;
+  b = A'*Beta;
+  a = A'*A;
+  dL= a\b;
+  C = C + dL(1);
+  x0 = x0 + dL(2);
+  sigX = sigX + dL(3);
+   %if FlagState==2;
+     % D = D + 0.01*dL(4); % Include damping term
+    %  fofX = C*exp(-(xx-x0).^2/(2*sigX^2)) + D; 
+  %else
+     fofX = C*exp(-(xx-x0).^2/(2*sigX^2));
+  %end;
+  Beta = yRows - fofX;
+  
+ % store iteration variables
+ Dfactorx(lpLSF,1,lpPx) = C;
+ Dfactorx(lpLSF,2,lpPx) = x0;
+ Dfactorx(lpLSF,3,lpPx) = sigX;
+ Dfactorx(lpLSF,4,lpPx) = ResBackground;
+ Dfactorx(lpLSF,5,lpPx) = sum(Beta.^2); 
+
+    if(abs(x0)>allowX || (sigX < -allowSig(1)) || (sigX > allowSig(2)) )
+     break; % Stop iterating if solution drifts too far
+    end
+  end
+  % Judge the fit. Accept if residue is a small proportion of |y^2|, etc.
+  residueRows = sum(Beta.^2)/sum(yRows.^2);
+  if (residueRows<tol && abs(x0)<allowX && sigX>allowSig(1) && sigX<allowSig(2))
+  fitRowPos = double(myRow)+x0-0.5; % Note (-0.5) for image registration
+  flagRowFits = true; % Flag the row-direction fit as acceptable
+  end
+ 
+  % Fit a Gaussian to the observed intensities, binned by Col
+  if(flagRowFits) % Don't fit col-direction if the row-axis fit was rejected
+  y0 = initX0;
+  sigY = sigX;% Keep sigX from row-fit. It should match the column-fit.
+  C = yCols(rad+1);
+ % if FlagState==2;
+ %   fofX = C*exp(-(yy-y0).^2/(2*sigY^2)) + D; % Initial guess of f(x)
+ %   d = ones  (size (xx,1),1);
+ % else 
+    fofX = C*exp(-(yy-y0).^2/(2*sigY^2));
+ % end
+  Beta = yCols - fofX; % Change needed in f(x)
+  
+  for lpLSF = 1:maxIts
+      
+% if FlagState==2;
+%    A = [fofX/C,fofX.*(yy-y0)/sigY^2,fofX.*(yy-y0).^2/sigY^3,d]; % Jacobian
+% else
+     A = [fofX/C,fofX.*(yy-y0)/sigY^2,fofX.*(yy-y0).^2/sigY^3];
+% end
+  b = A'*Beta;
+  a = (A'*A);
+  dL= a\b;
+  C = C+dL(1);
+  y0 = y0 + dL(2);
+   sigY = sigY + dL(3);
+ % if FlagState==2;
+ %   D = D + dL(4)*0.001;
+ %   fofX = C*exp(-(yy-y0).^2/(2*sigY^2))+ D; 
+ % else 
+    fofX = C*exp(-(yy-y0).^2/(2*sigY^2));
+ % end
+  Beta = yCols - fofX;
+ % Damping factor optimisation, store iteration variables
+  Dfactory(lpLSF,1,lpPx) = C;
+  Dfactory(lpLSF,2,lpPx) = y0;
+  Dfactory(lpLSF,3,lpPx) = sigY;
+  Dfactory(lpLSF,4,lpPx) = ResBackground;
+  Dfactory(lpLSF,5,lpPx) = sum(Beta.^2); 
+ 
+  if(abs(y0)>allowX || (sigY < allowSig(1)) || (sigY > allowSig(2)) )
+     break; % Stop iterating if solution drifts too far
+   end
+  end
+  % Judge the column direction fit:
+  residueCols = sum(Beta.^2)/sum(yCols.^2);
+   if (residueCols<tol && abs(y0)<allowX && sigY>allowSig(1) && sigY<allowSig(2))
+    fitColPos = double(myCol)+y0-0.5;
+    flagColFits = true; % Flag the column-direction fit as acceptable
+   end
+    end % End if, which only tries fitting Col-wise if Row-wise fitted OK
+ 
+ if(flagRowFits && flagColFits )     % Accept iff Row and Col fits are OK
+%   myFits(lpPx,:)=[fitRowPos,fitColPos];
+%   myParams(lpPx,1)=myPixels(lpPx,3); % Averaged magnitude of this signal
+%   myParams(lpPx,2)=(residueRows+residueCols)/2; % Mean critical tol for fit
+%   myParams(lpPx,3)=sum(yCols); % Sum of signal (counts) for this fit
+%   myParams(lpPx,4)=sigX;  % X-width (sigma, rows, fitted) of this Gaussian
+%   myParams(lpPx,5)=sigY;  % Y-width (sigma, cols, fitted) of this Gaussian
+%   myParams(lpPx,6)=bkgdSig;  % Background for each ROI
+
+  
+  SupResParams(index).frame_idx = frameIdx;
+  SupResParams(index).x = fitRowPos;
+  SupResParams(index).y = fitColPos;
+  SupResParams(index).z = 0;
+  SupResParams(index).I = myPixels(lpPx,3); % Averaged magnitude of this signal
+  SupResParams(index).I = myPixels(lpPx,3); % Averaged magnitude of this signal    
+  SupResParams(index).sig_x = sigX;  % X-width (sigma, rows, fitted) of this Gaussian
+  SupResParams(index).sig_y = sigY;  % Y-width (sigma, cols, fitted) of this Gaussian
+  SupResParams(index).avg_brigthness = bkgdSig; % Background for each ROI
+  SupResParams(index).res = (residueRows + residueCols) / 2; % Mean critical tol for fit
+  SupResParams(index).res_Row = residueRows;
+  SupResParams(index).res_Col = residueCols;
+  SupResParams(index).Sum_signal = sum(yCols); % Sum of signal (counts) for this fit
+% if FlagState==2
+%     SupResParams(index).Sum_signal = sum(yCols)-7*D; % Sum of signal (counts) for this fit
+% else
+    SupResParams(index).Sum_signal = sum(yCols); % Sum of signal (counts) for this fit
+%  end  
+  % Print D
+  %D
+  
+  index=index+1;
+ else 
+  Nfails = Nfails+1;
+ end
+ if( Nfails>2 )
+     break % After 3 failed fits, finish this frame (as Wolter-2010).
+ end
+ 
+end  % Loop to the next local maximum
+
+% myParams = myParams(myFits(:,1)~=-1,:); %Params of accepted fits only
+% myFits = myFits(myFits(:,1)~=-1,:); % Return accepted fits only. (-1)s are rejected fits.
+
+end
---
+++ rainSTORM_GUI_V2-41-MDC3/extras/Polarisation/Multiple_Molecules/rainSTORM_2BPolarizationAnalysisVM1.m
@@ -0,0 +1,289 @@
+%rainSTORM_2BPolarizationAnalysis
+%This script uses the main RainSTORM Software to analyse 2 stacks of x-y
+%polarized images
+
+% Made by Daan van Kleef & Mehdi Goudarzi
+
+clear 
+
+
+% Flagsetzero sets missing observations to count 10^-10
+% Flagsetbackground sets missing observations to background level
+% Flagbackgroundsubtraction subtracts background level from observed photon
+% counts and puts missing observations to 0
+% NOTE, do not use backgroundsubtraction and setbackground together!
+
+Flagsetzero = 0;
+Flagsetbackground = 0;
+Flagbackgroundsubtraction = 0;
+FlagrefitMin = 0;
+FlagrefitAverage= 1;
+
+% Setting the noise threshold. The number of succesful localisations per
+% pixel should exceed this value in order to be marked as a fluorophore
+% position.
+noiseThresh = 1;
+
+% Maximimum search radius to find local maximima in countmap to mark
+% fluorophore positions.
+LocRad = 1;
+
+% Maximim distance any localisation can be from a temporary molecule
+% location to be associated with that molecule.
+LocRange = 1;
+
+if sum(Flagsetzero+Flagsetbackground+Flagbackgroundsubtraction+FlagrefitMin+FlagrefitAverage) > 1
+    error('Make sure there is only one active flag!')
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1. Use rainSTORM to analyse raw image data
+%    Then read the data from the Matlab struct into a useful array...
+startup  % Run rainSTORM once (manually), on the X-polarisation data
+uiwait 
+
+% Read in as an array!
+Nxtable = struct2table((params.localization.results.SupResParams));
+
+for count = 1:size(Nxtable,2)
+    Nxdummy = double(table2array(Nxtable(:,count)));
+    if count == 1
+        Nxarray = Nxdummy;
+    else
+        Nxarray = cat(2,Nxarray,Nxdummy);
+    end
+end
+
+% Read the path of the stored stack. 
+XPath = params.rawdata_mgr.filename;
+
+
+startup  % Run rainSTORM once (manually), on the Y-polarisation data
+uiwait
+
+% Read in as an array!
+Nytable = struct2table((params.localization.results.SupResParams));
+for count = 1:size(Nytable,2)
+    Nydummy = double(table2array(Nytable(:,count)));
+    if count == 1
+        Nyarray = Nydummy;
+    else
+        Nyarray = cat(2,Nyarray,Nydummy);
+    end
+end
+
+% Read the path of the stored stack. 
+YPath = params.rawdata_mgr.filename;
+
+% 2.Quality control: This creates a density map for the frame and 
+frameSize = params.rawdata_mgr.myImInfo.frame_size (1);
+CountMapX = zeros (frameSize);
+
+NxCorrect = [Nxarray(:,1) Nxarray(:,2) Nxarray(:,3) Nxarray(:,12) -1*ones(size(Nxarray,1),2)];
+
+for countNarr = 1 : size (Nxarray, 1)
+    xtemp = floor(Nxarray(countNarr, 3)); 
+    ytemp = floor(Nxarray(countNarr, 2));
+    % All of the localisation positions are rounded down. 
+    CountMapX (xtemp, ytemp) = CountMapX (xtemp, ytemp) +1; 
+    % Creating the number of counts per pixel in the image to create a
+    % density map later. 
+end
+    
+%To remove most of the background noise. 
+CountMapX = CountMapX  - noiseThresh;
+CountMapX(CountMapX<0) = 0;
+
+% Determine preliminary locations of fluorophores, using avNMWS
+PrelimLoc = rainSTORM_avNMS(CountMapX,LocRad);
+
+%Now look for localisations within 2 pixels of the preliminary locations
+%(5x5 grid) to find the real average of the localisations. Then assign
+%localisations to the corresponding molecule
+
+for LocCount = 1:size(PrelimLoc,1);
+    for ArrayCount = 1:size(Nxarray,1);
+        Locdistance = sqrt((Nxarray(ArrayCount,3)-PrelimLoc(LocCount,1))^2+(Nxarray(ArrayCount,2)-PrelimLoc(LocCount,2))^2);
+        if Locdistance < LocRange
+         NxCorrect(ArrayCount,5) = PrelimLoc(LocCount,2);
+         NxCorrect(ArrayCount,6) = PrelimLoc(LocCount,1);
+        end
+    end
+end
+
+NxCorrect(NxCorrect(:, 5) == -1, :) = [];
+
+% Idem for Y-stack
+CountMapY = zeros (frameSize);
+NyCorrect = [Nyarray(:,1) Nyarray(:,2) Nyarray(:,3) Nyarray(:,12) -1*ones(size(Nyarray,1),2)];
+
+for countNarr = 1 : size (Nyarray, 1)
+    xtemp = floor(Nyarray(countNarr, 3)); 
+    ytemp = floor(Nyarray(countNarr, 2));
+    % All of the localisation positions are rounded down. 
+    CountMapY (xtemp, ytemp) = CountMapY (xtemp, ytemp) +1; 
+    % Creating the number of counts per pixel in the image to create a
+    % density map later. 
+end
+    
+%To remove most of the background noise. 
+CountMapY = CountMapY  - noiseThresh;
+CountMapY(CountMapY<0) = 0;
+
+% Determine preliminary locations of fluorophores, using avNMWS
+PrelimLoc = rainSTORM_avNMS(CountMapY,LocRad);
+
+%Now look for localisations within 2 pixels of the preliminary locations
+%(5x5 grid) to find the real average of the localisations. Then assign
+%localisations to the corresponding molecule
+
+for LocCount = 1:size(PrelimLoc,1);
+    for ArrayCount = 1:size(Nyarray,1);
+        Locdistance = sqrt((Nyarray(ArrayCount,3)-PrelimLoc(LocCount,1))^2+(Nyarray(ArrayCount,2)-PrelimLoc(LocCount,2))^2);
+        if Locdistance < LocRange
+         NyCorrect(ArrayCount,5) = PrelimLoc(LocCount,2);
+         NyCorrect(ArrayCount,6) = PrelimLoc(LocCount,1);
+        end
+    end
+end
+
+NyCorrect(NyCorrect(:, 5) == -1, :) = [];
+
+
+% 2. Identify matching images
+
+
+%Initiate matrix to hold combined count list
+% ComCount will hold the combined count list.
+% Column Structure:
+    %  Frame ID  | Nx | Ny | Nx present? | Ny present? | Estimated angle
+    % Last 2 column will hold flags to indicate succesful counts
+        % 0 = absent, 1 = present
+        % Can be used to relocalize missing observations later
+
+numberOfFrames = params.localization.results.numberOfFrames;
+ComCount = []; 
+
+for count = 1 : numberOfFrames
+        dumx = NxCorrect(find (NxCorrect(: , 1) == count),:);
+        dumy = NyCorrect(find (NyCorrect(: , 1) == count),:);
+        if (~isempty(dumx) && ~isempty(dumy)) 
+            dumx(:,[5,6,7]) = zeros(size(dumx,1),3);
+            dumy(:,[5,6]) = [];
+            for countX = 1:size(dumx,1);
+                for countY = 1:size(dumy,1)
+                    DistanceXY = sqrt((dumx(countX,2)-dumy(countY,2))^2+(dumx(countX,3)-dumy(countY,3))^2);
+                    if DistanceXY <= LocRange;
+                        dumx(countX, [5,6,7] ) = dumy(countY, [2, 3, 4]); 
+                        dumy(countY,1) = -1;
+                    end;
+                end;
+            end;
+            dumy(dumy(:, 1) == -1, :) = [];
+            dumy = [dumy(:,1) zeros(size(dumy,1),3) dumy(:,[2,3,4])];
+            dumx = cat(1, dumx, dumy);
+            if isempty(ComCount) 
+                ComCount = dumx;
+            else 
+                ComCount = cat (1, ComCount, dumx); 
+            end;
+        elseif isempty(dumy)
+            dumx(:,[5,6,7]) = zeros(size(dumx,1),3);
+            if isempty(ComCount) 
+                ComCount = dumx;
+            else 
+                ComCount = cat (1, ComCount, dumx); 
+            end;
+        elseif isempty(dumy)
+            dumy = [dumy(:,1) zeros(size(dumy,1),3) dumy(:,[2,3,4])];
+            if isempty(ComCount) 
+                ComCount = dumy;
+            else 
+                ComCount = cat (1, ComCount, dumy); 
+            end;
+        end;
+end;
+
+% 3. Complete missing paired observations
+
+% Method 1: Assume missing observations are count 10^-10
+if(Flagsetzero == 1);
+
+for count = 1:size(ComCount,1);
+    if ComCount(count, 4) == 0
+        ComCount (count , 4) = 10^(-10);
+         ComCount (count , [2,3]) = ComCount (count , [5,6]);
+    end
+    if ComCount(count,7) == 0
+        ComCount(count,7) = 10^(-10);
+        ComCount (count , [5,6]) = ComCount (count , [2,3]);
+    end
+end
+end
+
+% Method 2: Assume missing observations are background count level
+if(Flagsetbackground == 1);
+for count = 1:size(ComCount,1);
+    if ComCount(count, 4) == 0
+        ComCount (count , 4) = ResBackground;
+         ComCount (count , [2,3]) = ComCount (count , [5,6]);
+    end
+    if ComCount(count,7) == 0
+        ComCount(count,7) = ResBackground;
+        ComCount (count , [5,6]) = ComCount (count , [2,3]);
+    end
+end
+end
+
+% Method 3. Subtract average residual background level from measured
+%accepted intensities and put missing observations to 0
+% Legacy, currently not in use
+
+
+% Method 4: Re-inspect camera data for missing observations
+% (a) Find out which frames of which data file need to be inspected
+% (b) Process these and find region of interest
+% (c) Then use minimum of ROI as background noise value
+% (d) Use sum of ROI as missing observation
+% Legacy, currently not in use
+    
+
+% Method 5: Re-inspect camera data for missing observations
+% (a) Find out which frames of which data file need to be inspected
+% (b) Process these and find region of interest
+% (c) Then use average value outside of ROI as background noise value
+% (d) Use sum of ROI as missing observation
+if FlagrefitAverage == 1
+    for count = 1:size(ComCount, 1)
+        if ComCount(count, 4) == 0 
+               myFrame = double(imread([XPath,'.tif'], ComCount(count, 1)));
+               ComCount(count, [2,3]) = ComCount(count, [5,6]);
+               myROI= myFrame(ceil(ComCount(count,2))-3:ceil(ComCount(count,2))+3,ceil(ComCount(count,3))-3:ceil(ComCount(count,3))+3);
+               sum_signal = sum(myROI(:));
+               % Then pass sum_signal this into the relevant place (ComCount(number, X=2, Y=3) )
+               ComCount(count, 4) = sum_signal; 
+               
+        end
+        if ComCount(count, 7) == 0 
+             myFrame = double(imread([YPath,'.tif'], ComCount(count, 1)));
+             ComCount(count, [5,6]) = ComCount(count, [2, 3]);
+             myROI= myFrame(ceil(ComCount(count,2))-3:ceil(ComCount(count,2))+3,ceil(ComCount(count,3))-3:ceil(ComCount(count,3))+3);
+             sum_signal = sum(myROI(:));
+             % Then pass sum_signal this into the relevant place (ComCount(number, X=2, Y=3) )
+             ComCount(count, 7) = sum_signal; 
+                
+        end
+    end 
+end
+
+% 4. Caclulate polarization angle
+% Take negative angle if corrected counts are negative
+
+ComCount(:,8) = (180/pi)*acot(sqrt(abs(ComCount(:,4)./ComCount(:,7)))).*sign((ComCount(:,4)./ComCount(:,7)));
+
+
+
+%5. Conclusions
+% Results is a vector with the avergae estimated angle and the standard
+% deviation of the phi column (not error of the mean). 
+
